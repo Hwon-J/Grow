fd92b37fd685459d3e1ab3ba84812d4ac6a3e4a5
김태형 <sol20s@naver.com>
2023-07-26 15:49:04 +0900

    [FE]mod signuploginpage css수정: 완료x

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userSlice, { reducer as currentUser } from '../reducers/userSlice'; // userSlice.js에서 reducer를 명시적으로 가져옴
+import { reducer as currentUser } from '../reducers/userSlice'; // userSlice.js에서 reducer를 명시적으로 가져옴
+
+// store에 currentUser 변수 저장
 
 export default configureStore({
   reducer: {
