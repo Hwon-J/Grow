dc19903676488224f86a67d1f3c8f166ba69feb6
강신욱 <bearforce42@daum.net>
2023-07-27 14:06:06 +0900

    [BE] error : cors fixed
    cors 문제 수정

--- BackEnd Server/server.js
+++ BackEnd Server/server.js
@@ -19,9 +19,9 @@ app.use('/api/pot', potRoutes);
 const PORT = process.env.PORT;
 
 
-app.use( function(req, res, next) {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true");
         // next();
