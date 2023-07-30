05b7d5a9c441d50dedabe7d488d96023b615285e
강신욱 <bearforce42@daum.net>
2023-07-31 08:45:24 +0900

    [BE] error : cors error fixed
    cors 오류 고침
    - 코드 순서를 변경하는 것으로 해결

---
+++ BackEnd Server/app/controllers/sensor-controller.js
@@ -0,0 +1,34 @@
+const Sensor = require("../models/sensor-model.js");
+const connection = require("../config/connection.js");
+
+exports.checkSerial = async (req, res, next) => {
+  try {
+    let serial = req.params;
+    console.log(serial);
+
+    // 데이터베이스에서 멤버 조회
+    let query = "select * from `pot` where `serial_number`=?";
+
+    await connection.query(query, [serial.number], (error, result) => {
+      if (error) {
+        console.log("에러발생!!");
+        throw error;
+      }
+
+      // 시리얼 넘버 유효 체크
+      if (result.length == 0) {
+        return res.status(404).json({ message: "존재하지 않는 시리얼 넘버" });
+      }
+
+      // 시리얼 넘버 사용 여부 체크
+      if (result[0].member_index === null) {
+        res.status(200).json({ message: "사용 가능한 시리얼" });
+      } else {
+        res.status(401).json({ message: "이미 사용한 시리얼 넘버" });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "서버 오류" });
+  }
+};
