4b96a493e2b3e139c366c8fbcdb39dc69091b867
master <eoals6511@naver.com>
2023-08-01 21:37:38 +0900

    [FE] new : webpage, mod : display 웹페이지 홈화면에 추가해볼 영상 추가, 디스플레이 store 선언 위치 변경

--- display/display/src/App.js
+++ display/display/src/App.js
@@ -15,7 +15,7 @@ import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 // axios import
 import axios from 'axios';
 // store 관련 기능 import
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 import { Provider, useSelector, useDispatch} from 'react-redux';
 import WebSocketComponent from './components/Websocket';
 // useEffect
@@ -23,36 +23,19 @@ import { useEffect } from 'react';
 // 시리얼 번호 등록하는 컴포넌트 import 가져오기
 import SerialRegister from './components/SerialRegister';
 
-// 현재 store에 시리얼번호가 등록됐는지에 관한 변수를 바꿔주기 위해 리듀서 설정
-function serial_reducer(current_serial, action) {
-  // 현재 시리얼번호 등록을 모른다면 false값으로 변수 설정
-  if (current_serial === undefined) {
-    return {
-      serial_number : false
-    }
-  }
-  // action type으로 Success가 들어왔을 때 current_serial의 serial_number을 true로 변경
-  if (action.type === 'SUCCESS') {
-
-    const new_serial = {...current_serial}
-    new_serial.serial_number = true
-    console.log(new_serial)
-    return new_serial
-  }
-  
-  console.log('serial', current_serial)
-}
-// 만들어준 리듀서를 기반으로 store 생성
-const store = createStore(serial_reducer)
 
 // 캐릭터와 대화하는 화면 컴포넌트 생성
 function Conversation() {
   const state_serial_number = useSelector((state) => 
      state.serial_number
   )
+  const state_name = useSelector((state) =>
+    state.name
+  )
   // serial번호가 등록이 되었는지 확인 차원에서 렌더링 될 때 한번 출력
   useEffect(() => {
     console.log(state_serial_number); // 처음 렌더링 시에만 출력
+    // console.log(store)
   }, [])
 
   const character_style = {
@@ -63,6 +46,7 @@ function Conversation() {
   // 대화 컴포넌트 구조
   return (
     <div>
+      <h1>{state_name}</h1>
       <img src={flowercharacter} alt="cancel" style={character_style} />
       <img src={lettucecharacter} alt="cancel" style={character_style} />
       <img src={tomatocharacter} alt="cancel" style={character_style} />
@@ -89,14 +73,14 @@ function App() {
 
   return (
     // App주변으로 store설정
-    <Provider store={store}>
+    // <Provider store={store}>
       <div className="App" style={backgroundStyles}>
         <Routes>
           <Route path="/" element={<SerialRegister />} />
           <Route path="/conversation" element={<Conversation />} />
         </Routes>
       </div>
-    </Provider>
+    //  </Provider> 
   );
 }
 
