d5b5d313a391f1856c09d8c3032f60ffee644786
김태형 <sol20s@naver.com>
2023-08-01 10:54:54 +0900

    [FE]mod profilecardComponet:연결 프로필페이지 카드컴포넌트 연결

--- front-end/src/components/plant/PlantInfoComponent.js
+++ front-end/src/components/plant/PlantInfoComponent.js
@@ -4,7 +4,6 @@ import React from 'react';
 
 const PlantInfoComponent = (props) => {
   const prop = JSON.stringify(props)
-
   return (
     <div>
       {/* 해당 props의 정보가 있는지 없는지에 따라서 보여주는 화면 달라지게 */}
@@ -13,7 +12,6 @@ const PlantInfoComponent = (props) => {
       <h3>{prop.temperature_lower ? "최저온도 : " + prop.temperature_lower : ''}</h3>
       <h3>{prop.max_water_period ? "물이 필요한 날짜 상한 : " + prop.max_water_period : ''}</h3>
       <h3>{prop.difficulty ? "난이도 : " + prop.difficulty : '식물을 선택해 주세요'}</h3>
-
     </div>
   );
 }
--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,124 +1,86 @@
-// 김태형
-import React, {useEffect, useState} from 'react';
-import PlantCard from '../components/plantCard';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { useSelector } from 'react-redux';
-import NavTop from '../components/NavTop';
-import Footer from '../components/Footer';
+import React, { useEffect, useState } from "react";
+import PlantCard from "../components/plant/plantCard";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import NavTop from "../components/NavTop";
+import Footer from "../components/Footer";
+import "../components/plant/Profile.css";
 
-// 물 데이터 서버 켜졌을 때 자동으로 받아보는 코드
 const Profile = () => {
   const currentUser = useSelector((state) => state.currentUser);
   const authToken = currentUser.token;
   const navigate = useNavigate();
 
-  const [open, setOpen] = useState([]);
-  // 식물 데이터 서버 켜졌을 때 자동으로 받아보는 코드 
   const [plant, setPlant] = useState([]);
   const [pot, setPot] = useState([]);
-  const [growinPlant, setGrowinPlant] = useState([]);
-  const [plantComplete, setPlantComplete] = useState([]);
-  
+  const [growinPlant, setGrowinPlant] = useState([
+    {
+      plant_info: "상추",
+      plant_name: "상츠",
+      child_name: "김태형",
+      difficulty: 5,
+      imgname: 1,
+    },
+    {
+      plant_info: "상추",
+      plant_name: "상츠",
+      child_name: "김태형",
+      difficulty: 5,
+      imgname: 1,
+    },
+  ]);
+  const [plantComplete, setPlantComplete] = useState([
+    {
+      plant_info: "깻잎",
+      plant_name: "깻잌",
+      child_name: "김민국",
+      difficulty: 5,
+      imgname: 1,
+    },
+  ]);
 
-  useEffect(() => {
-    if (authToken !== "") {
-      waterPlanData();
-      plantData();
-      potData();
-    } else {
-      navigate('/login')
-    }
-  }, [authToken]);
-
-
-  const waterPlanData = async () => {
-    console.log(currentUser)
-    try {
-      const response = await axios.get('물준 axios주소', {
-        headers: {
-          Authorization: `Bearer ${authToken}` // 토큰을 'Bearer' 스킴으로 전송
-        }
-      });
-      setOpen(response.data);
-    } catch(err) {
-      console.log('에러가 발생', err);
-    }
-  };
-  // 화분 데이터 받아와서 화분데이터 저장
-  const potData = async () => {
-    try {
-      const response = await axios.get('화분 데이터 주소', {
-        headers: {
-          Authorization: `Bearer ${authToken}` // 토큰을 'Bearer' 스킴으로 전송
-        }
-      });
-      setPot(response.data);
-    } catch (error) {
-      console.log('에러', error);
-    }
-  };
-  // 식물 데이터 받아오기 >> 식물하나하나 비교해서 
-  // complete 가 0이면 키우는 중 || 1이면 완료된 식물이라 생각하고 데이터 저장
-  const plantData = async () => {
-    try {
-      const response = await axios.get('식물 데이터 axios주소');
-      setPlant(response.data)
-      setGrowinPlant(response.data.filter((plant) => plant.complete === 0))
-      setPlantComplete(response.data.filter((plant) => plant.complete === 1))
-    }
-    catch (error) {
-      console.log('에러', error)
-    }
-  }
-
-  // 현재 유저정보 state에서 가져오기
-  // const currentUser = useSelector(state => state.user.currentUser);
-  // 임시 유저정보
-  // const currentUser = {
-  //   name: '김태형',
-  //   email: 'sol20s@naver.com'
-  // };
-
-  // 키우는 중인 식물 카드 
   const NotcompleteCardSet = () => {
-    return growinPlant.map((plant) => {
-      return <PlantCard key={plant.id} props={plant} />; // plant의 고유한 id 값을 사용하여 key prop을 설정해주세요
-    });
+    return (
+      <div className="cardContainer">
+        {growinPlant.map((plant) => (
+          <PlantCard key={plant.id} props={plant} />
+        ))}
+      </div>
+    );
   };
-  
-  // 완료된 식물 카드
+
   const completeCardSet = () => {
-    return plantComplete.map((plant) => {
-      return <PlantCard key={plant.id} props={plant} />; // plant의 고유한 id 값을 사용하여 key prop을 설정해주세요
-    });
+    return (
+      <div className="cardContainer">
+        {plantComplete.map((plant) => (
+          <PlantCard key={plant.id} props={plant} />
+        ))}
+      </div>
+    );
   };
 
   return (
     <React.Fragment>
-      <NavTop/>
+      <NavTop />
       <div>
-        {/* authToken이 있을 때 */}
-        {authToken ? (
-          <div >
+        <div>
+          <div className="profile-left">
             <h1>Profile</h1>
-            <h2>name: {currentUser.name}</h2>
-            <h2>email: {currentUser.email}</h2>
-            <div className="container">
-              {completeCardSet()}
-            </div>
-            <div className="container">
+          </div>
+          <div className="profile-right">
+            <div>
+              <h2>키우는 식물</h2>
               {NotcompleteCardSet()}
             </div>
+            <div>
+              <h2>키운식물</h2>
+              {completeCardSet()}
+            </div>
           </div>
-        ) : null}
-
-        {/* authToken이 없을 때 */}
-        {!authToken && <div>
-          <h1>로그인 후 사용 해 주시오!</h1>
-          </div>}
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </React.Fragment>
   );
 };
