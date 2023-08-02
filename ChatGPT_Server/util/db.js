8e130e26e17e02efadbee7ff49776bf7e2f836a8
강신욱 <bearforce42@daum.net>
2023-08-02 16:51:41 +0900

    [BE] new : Add serial verification procedure to gpt server
    GPT 서버에 시리얼 확인절차 추가
    데이터 이름 변경: number -> serial

--- ChatGPT_Server/util/db.js
+++ ChatGPT_Server/util/db.js
@@ -1,17 +1,17 @@
 const maria = require("mysql");
 
 const connection = maria.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-  });
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
 
 // MariaDB connection 실행
-connection.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
-})
+connection.connect((error) => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
+});
 
 module.exports = connection;
