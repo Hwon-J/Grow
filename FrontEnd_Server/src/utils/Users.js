b6c9f597727f71fbe56d6e7466cd858f7193d372
김태형 <sol20s@naver.com>
2023-08-04 10:33:19 +0900

    [FE]mod 페이지 새로고침시 토큰 체크 : 토큰 유효성 검사 후 로그인 , 로그아웃 처리

---
+++ FrontEnd_Server/src/utils/Users.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const checkToken = async () => {
+  const persistedCurrentUser = localStorage.getItem('persist:currentUser');
+  const parsedCurrentUser = JSON.parse(persistedCurrentUser);
+  const token = JSON.parse(parsedCurrentUser.token);
+  console.log('Token:', token);
+
+
+  
+  const config = {
+    headers: {
+      Authorization: token,
+    },
+  };
+  try {
+    const response = await axios.post(
+      `http://192.168.100.37:30001/api/user/valid`,"", config
+    );
+    console.log(response)
+    if (response.status !== 200) {
+      localStorage.removeItem('persist:currentUser');
+      console.log('LocalStorage data removed.');
+    } 
+    
+  } catch (error) {
+    console.log(error);
+  }
+};
