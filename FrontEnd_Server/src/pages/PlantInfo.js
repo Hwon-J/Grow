a50cc886d22cb57ef8296c30ed0507c85d7f4512
김태형 <sol20s@naver.com>
2023-08-06 11:44:36 +0900

    [FE]error 처리

--- FrontEnd_Server/src/pages/PlantInfo.js
+++ FrontEnd_Server/src/pages/PlantInfo.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-
+import { BASE_URL } from "../utils/Urls";
 import {
   Alert,
   Button,
