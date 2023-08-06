c61bfc47442bf7ee5977738a4562bf042512bbb1
김민국 <eoals6511@naver.com>
2023-08-06 16:26:10 +0900

    [FE] mod:webpage/profile.js 프로필 페이지 css 수정

--- FrontEnd_Server/src/pages/Profile.js
+++ FrontEnd_Server/src/pages/Profile.js
@@ -19,54 +19,7 @@ import { FreeMode, Pagination } from "swiper/modules";
 import homevideo2 from '../assets/homevideo2.mp4';
 import { BASE_URL } from "../utils/Urls";
 
-function Example() {
-  const products = [
-    {
-      id: 1,
-      name: 'Basic Tee',
-      href: '#',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in black.",
-      price: '$35',
-      color: 'Black',
-    },
-    
-  ]
-  
-  return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">진행중인 식물</h2>
 
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                </div>
-                <p className="text-sm font-medium text-gray-900">{product.price}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  )
-}
 
 const Profile = () => {
   const currentUser = useSelector((state) => state.currentUser);
@@ -77,7 +30,7 @@ const Profile = () => {
   const [growinPlant, setGrowinPlant] = useState([
     {
       plant_info: "상추",
-      plant_name: "뷔",
+      plant_name: "싱싱이",
       child_name: "김태형",
       difficulty: 5,
       imgname: 1,
@@ -85,18 +38,66 @@ const Profile = () => {
     },
     {
       plant_info: "상추",
-      plant_name: "뷔vs",
+      plant_name: "추상이",
       child_name: "김태형",
       difficulty: 5,
       imgname: 2,
-      id: 72,
+      id: 1,
     },
     {
       plant_info: "상추",
-      plant_name: "귀요미",
+      plant_name: "마토",
       child_name: "김태형",
       difficulty: 5,
       imgname: 3,
+      id: 62,
+    },
+    {
+      plant_info: "상추",
+      plant_name: "닉넴추천",
+      child_name: "김태형",
+      difficulty: 5,
+      imgname: 4,
+      id: 1,
+    },
+    {
+      plant_info: "상추",
+      plant_name: "흰꽃",
+      child_name: "김태형",
+      difficulty: 5,
+      imgname: 5,
+      id: 62,
+    },
+    {
+      plant_info: "상추",
+      plant_name: "푸른이",
+      child_name: "김태형",
+      difficulty: 6,
+      imgname: 6,
+      id: 62,
+    },
+    {
+      plant_info: "상추",
+      plant_name: "빨갱이",
+      child_name: "김태형",
+      difficulty: 7,
+      imgname: 7,
+      id: 62,
+    },
+    {
+      plant_info: "상추",
+      plant_name: "이름없는꽃",
+      child_name: "김태형",
+      difficulty: 5,
+      imgname: 8,
+      id: 62,
+    },
+    {
+      plant_info: "상추",
+      plant_name: "미니나무",
+      child_name: "김태형",
+      difficulty: 5,
+      imgname: 9,
       id: 62,
     },
     
@@ -166,7 +167,8 @@ const Profile = () => {
     navigate('/register');
   }
   const [showInProgress, setShowInProgress] = useState(true);
-
+  
+  
   return (
     <div className="profile-total">
       <NavTop />
@@ -178,21 +180,25 @@ const Profile = () => {
               <h1 className="profile_title">PROFILE</h1>
             </div>
             <div>
-            <button className="btn btn-primary" onClick={() => setShowInProgress(true)}>진행중인 식물</button>
+            <button className="btn btn-primary"  onClick={() => {
+                setShowInProgress(true)
+            
+              }} >진행중인 식물</button>
             <span>  |  </span>
-            <button className="btn btn-success" onClick={() => setShowInProgress(false)}>완료된 식물</button>
+            <button className="btn btn-success" onClick={() => {
+                setShowInProgress(false)
+                
+              }} >완료된 식물</button>
             </div>
-            <Example />
+            
             <div className="container plant">
               {showInProgress && (<div className="plant-ing">
                 <div>
-                  <h2 >진행중인 식물</h2>
-                  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+                  
+                  
                   <Swiper
                     slidesPerView={3}
-                    spaceBetween={20}
+                    spaceBetween={30}
                     freeMode={true}
                     pagination={{
                       clickable: true,
@@ -218,7 +224,7 @@ const Profile = () => {
                 </div>
               </div>)}
               {!showInProgress && (<div className="plant-complete">
-                <h2>완료된 식물</h2>
+                
                 <Swiper
                   slidesPerView={3}
                   spaceBetween={20}
