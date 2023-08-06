6804ffcf523a20879c60cf4a21bfb3318e26991d
김태형 <sol20s@naver.com>
2023-08-06 18:35:21 +0900

    [FE]mod 회원탈퇴 , registerpage제거

--- FrontEnd_Server/src/pages/Profile.js
+++ FrontEnd_Server/src/pages/Profile.js
@@ -16,17 +16,19 @@ import "swiper/css/pagination";
 // import required modules
 import { FreeMode, Pagination } from "swiper/modules";
 
-import homevideo2 from '../assets/homevideo2.mp4';
+import homevideo2 from "../assets/homevideo2.mp4";
 import { BASE_URL } from "../utils/Urls";
-
-
 
 const Profile = () => {
   const currentUser = useSelector((state) => state.currentUser);
   const token = currentUser.token;
   console.log(token);
   const navigate = useNavigate();
-
+  const config = {
+    headers: {
+      Authorization: token,
+    },
+  };
   const [growinPlant, setGrowinPlant] = useState([
     {
       plant_info: "상추",
@@ -100,7 +102,6 @@ const Profile = () => {
       imgname: 9,
       id: 62,
     },
-    
   ]);
   const [plantComplete, setPlantComplete] = useState([
     {
@@ -112,25 +113,31 @@ const Profile = () => {
       id: 3,
     },
   ]);
-
+  const withdrawal = async () => {
+    try {
+      const response = await axios.post(`${BASE_URL}/api/user/`, "", config);
+      alert(response.data.message);
+      navigate("/signup");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   // 식물 종 데이터 변경할 메서드
   const getPlants = async () => {
-    console.log("보내기 "+token)
-    const config = {
-      headers: {
-        Authorization: token, 
-      },
-    };
+    console.log("보내기 " + token);
     try {
-      const response = await axios.get(`${BASE_URL}/api/plant/myplant/`,config);
-      console.log(response.data.data)
+      const response = await axios.get(
+        `${BASE_URL}/api/plant/myplant/`,
+        config
+      );
+      console.log(response.data.data);
       const plantsList = response.data.data;
       const growing = plantsList.filter((plant) => plant.complete === 0);
       const complete = plantsList.filter((plant) => plant.complete === 1);
       setGrowinPlant(growing);
       setPlantComplete(complete);
-      console.log(growinPlant)
-      console.log(plantComplete)
+      console.log(growinPlant);
+      console.log(plantComplete);
     } catch (error) {
       console.log(error);
     }
@@ -164,41 +171,84 @@ const Profile = () => {
   };
 
   const createCard = () => {
-    navigate('/register');
-  }
+    navigate("/plantinfo");
+  };
   const [showInProgress, setShowInProgress] = useState(true);
-  
-  
+
   return (
     <div className="profile-total">
       <NavTop />
       <div className="profilepage container">
         <div className="profile-row">
-          
           <div className="container profile row">
             <div className="col-12">
               <h1 className="profile_title">PROFILE</h1>
             </div>
-            <div>
-            <button className="btn btn-primary"  onClick={() => {
-                setShowInProgress(true)
-            
-              }} >진행중인 식물</button>
-            <span>  |  </span>
-            <button className="btn btn-success" onClick={() => {
-                setShowInProgress(false)
-                
-              }} >완료된 식물</button>
+            <div class="button-container">
+              <div>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => setShowInProgress(true)}
+                >
+                  진행중인 식물
+                </button>
+                <span> | </span>
+                <button
+                  className="btn btn-success"
+                  onClick={() => setShowInProgress(false)}
+                >
+                  완료된 식물
+                </button>
+              </div>
+              <button className="btn btn-danger signout" onClick={withdrawal}>
+                회원탈퇴
+              </button>
             </div>
-            
+
             <div className="container plant">
-              {showInProgress && (<div className="plant-ing">
-                <div>
-                  
-                  
+              {showInProgress && (
+                <div className="plant-ing">
+                  <div>
+                    <Swiper
+                      slidesPerView={4}
+                      spaceBetween={30}
+                      freeMode={true}
+                      pagination={{
+                        clickable: true,
+                      }}
+                      modules={[FreeMode, Pagination]}
+                      className="mySwiper"
+                    >
+                      {NotcompleteCardSet()}
+                      <SwiperSlide>
+                        <div className="plantcard">
+                          <div className="bg">
+                            <div className="plantcard-details">
+                              <h5>
+                                새 식물친구
+                                <br />
+                                등록해주기
+                              </h5>
+                            </div>
+                          </div>
+                          <div className="blob"></div>
+                          <button
+                            className="button type1 plantcard-button"
+                            onClick={createCard}
+                          >
+                            <span className="btn-txt">+</span>
+                          </button>
+                        </div>
+                      </SwiperSlide>
+                    </Swiper>
+                  </div>
+                </div>
+              )}
+              {!showInProgress && (
+                <div className="plant-complete">
                   <Swiper
-                    slidesPerView={4}
-                    spaceBetween={30}
+                    slidesPerView={3}
+                    spaceBetween={20}
                     freeMode={true}
                     pagination={{
                       clickable: true,
@@ -206,46 +256,15 @@ const Profile = () => {
                     modules={[FreeMode, Pagination]}
                     className="mySwiper"
                   >
-                    {NotcompleteCardSet()}
-                    <SwiperSlide>
-                      <div className="plantcard">
-                        <div className="bg">
-                          <div className="plantcard-details" >
-                            <h5>새 식물친구<br/>등록해주기</h5>
-                          </div>
-                        </div>
-                        <div className="blob"></div>
-                        <button className="button type1 plantcard-button" onClick={createCard}>
-                          <span className="btn-txt">+</span>
-                        </button>
-                      </div>
-                    </SwiperSlide>
+                    {completeCardSet()}
                   </Swiper>
                 </div>
-              </div>)}
-              {!showInProgress && (<div className="plant-complete">
-                
-                <Swiper
-                  slidesPerView={3}
-                  spaceBetween={20}
-                  freeMode={true}
-                  pagination={{
-                    clickable: true,
-                  }}
-                  modules={[FreeMode, Pagination]}
-                  className="mySwiper"
-                >
-                  {completeCardSet()}
-
-                </Swiper>
-              </div>)}
-              
-              </div>
+              )}
             </div>
           </div>
+        </div>
       </div>
-      </div>
-    
+    </div>
   );
 };
 
