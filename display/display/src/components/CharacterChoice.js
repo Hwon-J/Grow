3554334578a6b414c7bd0fd93d85c1071085c5c4
김민국 <eoals6511@naver.com>
2023-08-07 17:16:11 +0900

    [FE] new : characterchoice.js 대화 창에 가기전에 캐릭터를 고르는 화면 추가

---
+++ display/display/src/components/CharacterChoice.js
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import './CharacterChoice.css';
+import lettucecharacter from '../assets/lettucecharacter.png';
+
+import beancharacter from '../assets/beancharacter.png';
+import tomatocharacter from '../assets/tomatocharacter.png';
+import logo from '../assets/logo.png';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+
+export default function CharacterChoice() {
+    // navigate와 dispatch를 사용하기 위해 기능 변수 설정
+    const navigate = useNavigate();
+    const base_serial_number = 15648
+    const character_list = [beancharacter, lettucecharacter, tomatocharacter]
+    
+    
+    return (
+      <div className='characterchoice'>
+        <div>
+
+        <h1> 마음에 드는 캐릭터를 골라주세요. </h1>
+        {character_list.map((character, index) => (
+          <img key={index} src={character} alt={`Character ${index}`} 
+          onClick={() => {
+            navigate(`/conversation/${index}/${base_serial_number}`)
+          }}/>
+          ))}
+        </div>
+      </div>
+    )
+  
+  }
+
