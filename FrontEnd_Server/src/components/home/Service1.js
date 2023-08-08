b68142c08f2a3b8ba07187e338bd491af0d5c60d
김태형 <sol20s@naver.com>
2023-08-08 22:56:28 +0900

    [FE]new : homepage아래로 내리는 페이지 제작

--- FrontEnd_Server/src/components/home/Service1.js
+++ FrontEnd_Server/src/components/home/Service1.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-import './service1.css';
+import "./service1.css";
 
 const Service1 = () => {
   const [showChildImage, setShowChildImage] = useState(false);
@@ -18,14 +18,18 @@ const Service1 = () => {
     <div className="container service1-main-div">
       <Row className="service1-row">
         <Col className="service1-col col1-service" xs={7}>
-          <img src={`./home/home2.png`}/>
-          {showChildImage && (
-            <img className="child-img" src={`./home/child1.png`} />
-          )}
+          <img src={`./home/child5.jpg`} style={{ width: "100%" }} />
         </Col>
-        <Col className="service2-col col2-service" xs={5}>
-          <h2>아이의 속마음</h2><br/>
-          <h2>아이의 비밀친구에게 속삭여봐요!</h2>
+        <Col className="service2-col col2-service right-col" xs={5}>
+          <div>
+            <p className="col2-p">식물과 함께 성장</p>
+            <br />
+          </div>
+          <h2 className="col2-h2">집에서도 외롭지 않게</h2>
+          <br />
+          <div>
+            <p className="col2-p2">함께 성장하는 식물 친구와 함께</p>
+          </div>
         </Col>
       </Row>
     </div>
