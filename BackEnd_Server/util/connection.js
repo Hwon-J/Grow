621fb77390459e9b4dee78e273f9f44a2de83560
강신욱 <bearforce42@daum.net>
2023-08-11 10:43:57 +0900

    [BE] new : add S3 server connection
    S3 서버와 연결 생성

--- BackEnd_Server/util/connection.js
+++ BackEnd_Server/util/connection.js
@@ -1,18 +1,38 @@
 const maria = require("mysql");
 const winston = require("./winston.js");
 
-const connection = maria.createConnection({
+const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
-});
+};
 
-// MariaDB connection 실행
-connection.connect(error => {
-    if (error) throw error;
-    winston.info("Successfully connected to the database.");
-})
+let connection;
+
+function handleDisconnect() {
+  connection = maria.createConnection(dbConfig);
+
+  connection.connect(err => {
+    if (err) {
+      winston.error('Error connecting to the database:', err);
+      setTimeout(handleDisconnect, 2000);  // 2초 후에 재연결 시도
+    } else {
+      winston.info("Successfully connected to the database.");
+    }
+  });
+
+  connection.on('error', err => {
+    winston.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();  // 연결 끊김 시 재연결
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
